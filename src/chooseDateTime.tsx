18070cf012c8fa8b9eba374cf03c6724500ba0ea
deepakSRawat <deepsrawat501@gmail.com>
2025-12-06 11:05:12 +0530

    add tgz in gitignore seperate date and time field add svg icon in dist add margin bottom on every field

--- src/chooseDateTime.tsx
+++ src/chooseDateTime.tsx
@@ -1,37 +1,15 @@
 import DateTimePicker from "@react-native-community/datetimepicker";
 import React, { useState } from "react";
 import { Controller } from "react-hook-form";
-import { Pressable, Text, View } from "react-native";
-type ShowType = {
-	showVal: boolean;
-	type: "date" | "time";
-};
+import { Pressable, Text, TextInput, View } from "react-native";
+import { Calendar, Clock } from "./assets/icon";
 
 const DateTimeType = ({ field, control, errors }: any) => {
 	const [date, setDate] = useState<Date | null>();
-	const [show, setShow] = useState<ShowType>({
-		showVal: false,
-		type: "date",
-	});
+	const [time, setTime] = useState<Date | null>();
+	const [showClock, setShowClock] = useState(false);
+	const [showCalender, setShowCalender] = useState(false);
 
-	const showDatePicker = () => {
-		setShow((prev) => ({ ...prev, showVal: true }));
-	};
-	const onChangeHandler = (event: any, selectedDate: any) => {
-		const currentDate = selectedDate || date;
-		//console.log("selected time:", currentDate)
-
-		//const nDate = convertToIST(currentDate)
-
-		//console.log('Date: ----- >>> ', nDate?.date);
-
-		setDate(currentDate);
-		if (show.type === "date") {
-			setShow((prev) => ({ ...prev, type: "time" }));
-			return;
-		}
-		setShow({ type: "date", showVal: false });
-	};
 	return (
 		<Controller
 			control={control}
@@ -42,48 +20,159 @@ const DateTimeType = ({ field, control, errors }: any) => {
 			render={({ field: { onChange: fieldOnChange } }: any) => {
 				return (
 					<>
-						<View>
-							<Text>
-								{field.label}:
-								<Text>{field.required ? "*" : ""}</Text>
-							</Text>
+						{field.type === "date" && (
 							<View>
-								<Text onPress={showDatePicker}>
-									{date
-										? `${date.toLocaleString("en-US", {
-												year: "numeric",
-												month: "short",
-												day: "numeric",
-												hour: "2-digit",
-												minute: "2-digit",
-												weekday: "long",
-										  })}`
-										: "Choose Date & Time"}
+								<Text>
+									{field.label}:
+									<Text style={{ color: "red" }}>
+										{field.required ? "*" : ""}
+									</Text>
 								</Text>
-								<Pressable onPress={showDatePicker}></Pressable>
-								{/* <Button title="Enter date and time" /> */}
-								{show.showVal && (
-									<DateTimePicker
-										value={date ?? new Date()}
-										mode={show.type} // Change to 'time' for time picker
-										display="default"
-										onChange={(event, selectedDate) => {
-											onChangeHandler(
-												event,
-												selectedDate
-											);
-											fieldOnChange(selectedDate);
+								<View
+									style={{
+										borderWidth: 1,
+										borderRadius: 5,
+										borderColor: errors[field.name]
+											? "red"
+											: "#ccc",
+										flexDirection: "row", // Row layout
+										alignItems: "center",
+									}}
+								>
+									<TextInput
+										showSoftInputOnFocus={false}
+										placeholder={field.placeholder}
+										onPress={() => setShowCalender(true)}
+										style={{
+											flex: 1,
+											padding: 10,
 										}}
+										value={
+											date
+												? `${date.toLocaleString(
+														"en-US",
+														{
+															year: "numeric",
+															month: "short",
+															day: "numeric",
+														}
+												  )}`
+												: ""
+										}
 									/>
+									<Pressable
+										style={{
+											alignItems: "flex-end",
+											padding: 10,
+										}}
+										onPress={() => setShowCalender(true)}
+									>
+										<Calendar width={20} height={20} />
+									</Pressable>
+									{showCalender && (
+										<DateTimePicker
+											design="material"
+											value={date ?? new Date()}
+											mode={"date"} // Change to 'time' for time picker
+											display="default"
+											onChange={(event, selectedDate) => {
+												setDate(selectedDate);
+												fieldOnChange(selectedDate);
+												setShowCalender(false);
+											}}
+										/>
+									)}
+								</View>
+								{typeof errors[field.name]?.message ===
+									"string" && (
+									<Text
+										style={{
+											color: "red",
+											marginBottom: 8,
+										}}
+									>
+										{errors[field.name]?.message as string}
+									</Text>
 								)}
 							</View>
-							{typeof errors[field.name]?.message ===
-								"string" && (
-								<Text style={{ color: "red", marginBottom: 8 }}>
-									{errors[field.name]?.message as string}
+						)}
+						{field.type === "time" && (
+							<View>
+								<Text>
+									{field.label}:
+									<Text style={{ color: "red" }}>
+										{field.required ? "*" : ""}
+									</Text>
 								</Text>
-							)}
-						</View>
+								<View
+									style={{
+										borderWidth: 1,
+										borderRadius: 5,
+										borderColor: errors[field.name]
+											? "red"
+											: "#ccc",
+										flexDirection: "row", // Row layout
+										alignItems: "center",
+									}}
+								>
+									<TextInput
+										showSoftInputOnFocus={false}
+										onPress={() => setShowClock(true)}
+										placeholder={field.placeholder}
+										style={{
+											flex: 1,
+											padding: 10,
+										}}
+										value={
+											time
+												? `${time.toLocaleString(
+														"en-US",
+														{
+															hour: "2-digit",
+															minute: "2-digit",
+														}
+												  )}`
+												: ""
+										}
+									/>
+
+									<Pressable
+										style={{
+											alignItems: "flex-end",
+											padding: 10,
+										}}
+										onPress={() => setShowClock(true)}
+									>
+										<Clock width={20} height={20} />
+									</Pressable>
+
+									{showClock && (
+										<DateTimePicker
+											design="material"
+											value={time ?? new Date()}
+											mode={"time"} // Change to 'time' for time picker
+											display="default"
+											onChange={(event, selectedTime) => {
+												setTime(selectedTime);
+												fieldOnChange(selectedTime);
+												setShowClock(false);
+											}}
+										/>
+									)}
+								</View>
+								{typeof errors[field.name]?.message ===
+									"string" && (
+									<Text
+										style={{
+											color: "red",
+											marginBottom: 8,
+										}}
+									>
+										{errors[field.name]?.message as string}
+									</Text>
+								)}
+							</View>
+						)}
 					</>
 				);
 			}}
